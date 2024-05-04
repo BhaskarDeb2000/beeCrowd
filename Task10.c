60bb0a3a45ef3e48410ece495ed583500f8864e9
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-04 17:59:45 +0300

    doc: Salary with Bonus -- 04.05.2024

--- Task10.c
+++ Task10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    char firstName[20];
+    char firstName[15];
     double salary, payPerHour;
     scanf("%s\n%lf\n%lf", firstName, &salary, &payPerHour);
     printf("TOTAL = R$ %.2f\n", salary + (payPerHour * 0.15));
