1842641c16affe00aba6807737208ab6ef0fb3dc
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-04 17:46:53 +0300

    doc: Simple Sum -- 04.05.2024

--- Task4.c
+++ Task4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int A, B, SOMA;
-    scanf("%d", &A);
-    scanf("%d", &B);
-    SOMA = A + B;
+    int a, b, SOMA;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    SOMA = a + b;
 
     printf("SOMA = %d\n", SOMA);
 
