2132f16457ed9aaac591fb62f73309ee71f75d78
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-05 10:06:16 +0300

    doc: salary calculator -- 04.05.2024

---
+++ Task9.c
@@ -0,0 +1,14 @@
+// Salary calculator
+
+#include <stdio.h>
+int main()
+{
+    int id, workHour;
+    double payPerHour, salary;
+    scanf("%d %d %lf", &id, &workHour, &payPerHour);
+
+    salary = workHour * payPerHour;
+    printf("NUMBER = %d\n", id);
+    printf("SALARY = U$ %.2lf\n", salary);
+    return 0;
+}
