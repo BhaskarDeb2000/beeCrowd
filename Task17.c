251f364d70deab303dd5bf8cab0117c0b6984303
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-05 17:16:37 +0300

    doc: time that takes to reach a distance -- 05.05.2024

---
+++ Task17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int distance;
+    scanf("%d", &distance);
+    int timeTakes = distance * 2;
+    printf("%d minutos\n", timeTakes);
+    return 0;
+}
