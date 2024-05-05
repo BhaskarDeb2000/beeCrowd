0f16db257a568869f0393d0637bf573c877e3fd8
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-05 10:07:50 +0300

    doc: simple product -- 04.05.2024

---
+++ Task5.c
@@ -0,0 +1,13 @@
+// Simple Product
+
+#include <stdio.h>
+int main()
+{
+    int A, B, PROD;
+    scanf("%d\n%d\n", &A, &B);
+    PROD = A * B;
+
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
