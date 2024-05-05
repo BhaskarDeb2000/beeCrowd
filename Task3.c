fc97f4d622b81427b7ab855d33e8771701a527f1
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-05 10:08:31 +0300

    doc: area of circle -- 04.05.2024

---
+++ Task3.c
@@ -0,0 +1,12 @@
+// Area of Circle
+
+#include <stdio.h>
+int main()
+{
+    double a, result;
+    const double x = 3.14159;
+    scanf("%lf", &a);
+    result = x * a * a;
+    printf("A=%.4f\n", result);
+    return 0;
+}
