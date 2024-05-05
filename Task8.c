3db17ed72f886930e3911fd84e0df6ca7c6d8613
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-05-05 10:06:45 +0300

    doc: difference with multiplication -- 04.05.2024

---
+++ Task8.c
@@ -0,0 +1,11 @@
+// Difference with multiplication
+
+#include <stdio.h>
+int main()
+{
+    int A, B, C, D, DIFERENCE;
+    scanf("%d\n%d\n%d\n%d", &A, &B, &C, &D);
+    DIFERENCE = (A * B) - (C * D);
+    printf("DIFERENCA = %d\n", DIFERENCE);
+    return 0;
+}
